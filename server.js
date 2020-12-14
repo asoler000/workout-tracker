dcb53abc0156870257925e92ec8760b87afae571
asoler000 <68714690+asoler000@users.noreply.github.com>
2020-12-14 13:45:34 -0600

    Update server.js
    updated server file hopefully this works

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(morgan("tiny"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
